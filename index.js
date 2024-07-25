5ad615ea75f5325fdc592b0227ae123eb6eb242e
JellyFished <gisele0kms@gmail.com>
2024-07-25 11:37:19 -0400

    review the Phase

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
-// Write your code here
+// is an equation whose multiplied results will equal 62
+
+let num1 = 31;
+let num2 = 2;
+
+
+
+const multiply = (num1 *= num2);
+
+
+
+// generates a random integer greater than 0 
+
+const random = Math.ceil(Math.random());
+
+
+// is an equation that calculates a remainder that is equal to 4 
+
+let num3 = 9;
+let num4 = 5;
+
+const mod = (num3 % num4);
+
+
+// will return 20 as the highest number in the set
+
+const max = Math.max(3, 6, 11, 18, 20);
